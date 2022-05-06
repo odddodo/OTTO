51335525190ea499b277712b065f17ed61ec82e9
odddodo <dodo@wiedenski.org>
2022-05-06 14:00:21 +0200

    listening to incoming serial

--- src/functions.h
+++ src/functions.h
@@ -12,6 +12,8 @@ void initHardware(){
 
 Serial.begin(SERIALBAUDRATE);
 
+Serial3.begin(SERIALBAUDRATE);
+
 pinMode(GAS135_D,INPUT);
 pinMode(GAS2_D,INPUT);
 pinMode(DEBUG_LED,OUTPUT);
@@ -27,18 +29,43 @@ void blink(){
 }
 
 void collectSensorData(){
-collectedData[0]=analogRead(PARTICLE);
-collectedData[1]=analogRead(GAS135_A);
-collectedData[2]=analogRead(GAS2_A);
-collectedData[3]=analogRead(UV);
+collectedData[DUST_VAL]=analogRead(PARTICLE);
+collectedData[GAS135_VAL]=analogRead(GAS135_A);
+collectedData[GAS2_VAL]=analogRead(GAS2_A);
+collectedData[UV_VAL]=analogRead(UV);
 }
 
 void outputData2Serial(){
-DEBUG(String(collectedData[0])+","+String(collectedData[1])+","+String(collectedData[2])+","+String(collectedData[3]));
+DEBUG(String(collectedData[DUST_VAL])
++","+String(collectedData[GAS135_VAL])
++","+String(collectedData[GAS2_VAL])
++","+String(collectedData[UV_VAL])
++","+String(collectedData[4])
++","+String(collectedData[5]));
+}
+
+const int BUFFER_SIZE =6;
+
+void listenToWeatherShield(){
+    char buf[BUFFER_SIZE];
+    if(Serial3.available()>0){
+        
+        Serial3.readBytesUntil('w',buf,BUFFER_SIZE);
+      
+            collectedData[4]=(int)buf[0];
+            collectedData[5]=(int)buf[1];
+        
+        
+    }
+    else{
+        collectedData[4]=0;
+        
+    }
 }
 
 Ticker blinkingSlow(blink,ALRIGHTBLINK);
 Ticker blinkingFast(blink,ERRORBLINK);
 Ticker collect(collectSensorData,ACQUISITION_FREQUENCY);
 Ticker report(outputData2Serial,REPORTING_FREQUENCY);
+Ticker listen(listenToWeatherShield,100);
 #endif
--- src/globals.h
+++ src/globals.h
@@ -4,8 +4,8 @@
 
 #define SERIALBAUDRATE 9600
 
-#define ACQUISITION_FREQUENCY 5000
-#define REPORTING_FREQUENCY 5000
+#define ACQUISITION_FREQUENCY 1000
+#define REPORTING_FREQUENCY 1000
 
 #define PARTICLE A0
 #define SONAR_1 22
@@ -15,6 +15,20 @@
 #define GAS2_A A2
 #define GAS2_D 25
 #define UV A3
+
+//POSITION OF ALL THE DATA IN THE ARRAY
+#define DUST_VAL 0
+#define SONAR1_VAL 1
+#define SONAR2_VAL 2
+#define GAS135_VAL 3
+#define GAS2_VAL 4
+#define UV_VAL 5
+#define WIND_VAL 6
+#define WIND_DIR_VAL 7
+#define RAIN_VAL 8
+#define HUMIDITY 9
+#define PRESSURE 10
+#define LIGHT 11
 
 #define DEBUG_LED 13
 
